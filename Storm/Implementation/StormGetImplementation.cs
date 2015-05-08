d50da783e81032fced1fd54499216b7fd944b767
vladimir-repin <vladimir.repin@exigenservices.com>
2015-05-08 15:36:54 +0300

    added source items cloning

--- Storm/Implementation/StormGetImplementation.cs
+++ Storm/Implementation/StormGetImplementation.cs
@@ -3,6 +3,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using St.Orm.Interfaces;
+    using St.Orm.Interfaces.Internal;
     using St.Orm.Parameters;
 
     internal static class StormGetImplementation
@@ -12,7 +13,10 @@
             var parametersDictionary = parameters.ToDictionary(x => x.Key, x => x.Value);
             var loadService = new LoadService<TDal>(parametersDictionary, context);
             var repo = RepositoryStorage.GetRepository<TDal>();
-            return repo.Materialize(query, loadService);
+            var items = repo.Materialize(query, loadService);
+            var result = new List<TDal>(items.Count);
+            result.AddRange(items.Select(x => (x as IAmLazy<TDal>).Clone()));
+            return result;
         }
     }
 }
