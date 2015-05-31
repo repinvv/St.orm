454d63cbfdb9eb132f06f2e3c4f12673872aaf2b
vladimir repin <repin.vladv@gmail.com>
2015-05-31 22:32:44 +0300

    minor

--- StormGenerator/ModelsCollection/ModelsCollector.cs
+++ StormGenerator/ModelsCollection/ModelsCollector.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StormGenerator.ModelsCollection
+﻿namespace StormGenerator.ModelsCollection
 {
     class ModelsCollector
     {
