38d3f5d7ccb87fb7da26e87e8aa59c7a6a5fa388
vladimir repin <repin.vladv@gmail.com>
2015-05-31 22:27:51 +0300

    minor naming

--- StormGenerator/ModelsCollection/NameCreator.cs
+++ StormGenerator/ModelsCollection/NameCreator.cs
@@ -14,7 +14,12 @@
 
         private string ConvertSection(string arg)
         {
-            return char.ToUpper(arg[0]) + arg.Substring(1).ToLower();
+            return char.ToUpper(arg[0]) + ConvertRestOfString(arg.Substring(1));
+        }
+
+        private string ConvertRestOfString(string arg)
+        {
+            return arg.All(char.IsUpper) || arg.All(char.IsLower) ? arg.ToLower() : arg;
         }
 
         public string CreatePluralName(string name)
