bfa7dfd64ea0ac6b77ce2c188c33d259c1e6655b
vladimir repin <repin.vladv@gmail.com>
2015-06-27 23:16:59 +0300

    started saving

--- Storm/Storm.cs
+++ Storm/Storm.cs
@@ -1,5 +1,6 @@
 ﻿namespace St.Orm
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using St.Orm.Implementation;
@@ -17,6 +18,37 @@
         {
             var query = context.GetDalRepository<TDal, TDal>().GetByIdQuery(id, context);
             return StormGetImplementation.Get(query, context, parameters).SingleOrDefault();
+        }
+
+        public static void Save<T>(ICollection<T> entities, IStormContext context)
+        {
+            if (entities == null)
+            {
+                throw new ArgumentException("entities");
+            }
+
+            var saves = new SavesCollector();
+            var list = entities.AsList();
+            var existing = new List<T>(entities.Count);
+            foreach (var entity in entities)
+            {
+                existing.Add((entity as ICloneable<T>).ClonedFrom());
+            }
+                
+            saveService.UpdateEntities(list, existing, saves, null);
+            saves.Commit(context);
+        }
+
+        public static void SaveEntities<T>(ICollection<T> entities, ICollection<T> existing, IStormContext context)
+        {
+            if (entities == null)
+            {
+                throw new ArgumentException("entities");
+            }
+
+            var saves = new SavesCollector();
+            saveService.UpdateEntities(entities.AsList(), existing.AsList(), saves, null);
+            saves.Commit(context);
         }
     }
 }
